119875ab4b116892c7eb72ee8792b32ba3a0d208
Ji Chung <jwchungp@gmail.com>
2021-02-08 16:59:50 -0800

    moving tasks to a above the hierachry tree and added react icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState ([
+    {
+      id: 1,
+      text: "Last Day At Experian",
+      day: "Feb 11th",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "First Day at Acorns",
+      day: "Feb 22th",
+      reminder: true,
+    }
+  ])
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,10 @@
-import { useState } from 'react'
+import Task from './Task'
 
-const Tasks = (props) => {
-  const [tasks, setTasks] = useState ([
-    {
-      id: 1,
-      text: "Last Day At Experian",
-      day: "Feb 11th",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "First Day at Acorns",
-      day: "Feb 22th",
-      reminder: true,
-    }
-  ])
-
+const Tasks = ({tasks}) => {
   return (
     <>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task}/>
       ))}
     </>
   );
