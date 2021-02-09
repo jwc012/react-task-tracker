db803f276ddad0e33020c75fb0953ecbd830bd9b
Ji Chung <jwchungp@gmail.com>
2021-02-08 16:14:15 -0800

    added css styling and various other components

---
+++ src/components/Button.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+
+const Button = ({color, text, onClick}) => {
+
+  return (
+    <button 
+      onClick={onClick}
+      style={{backgroundColor: color}} 
+      className='btn'>{text}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+  text: 'Add',
+  color: 'steelblue'
+}
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired, 
+  color: PropTypes.string,
+  onClick: PropTypes.func,
+}
+
+export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({title}) => {
+  const onClick = () => {
+    console.log('click')
+  }
+
   return (
-    <header>
+    <header className='header'>
       <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick}/>
     </header>
   )
 }
@@ -15,5 +21,11 @@ Header.defaultProps = {
 Header.propTypes = {
   title: PropTypes.string.isRequired,
 }
+
+// CSS in JS
+// const headingStyle = {
+//   color: 'red', 
+//   backgroundColor: 'black'
+// }
 
 export default Header
